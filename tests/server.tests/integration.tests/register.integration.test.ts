a9817413a7a6eb23d5c08177a9c8e9da8f412fd9
George A Stey <gstey@andrew.cmu.edu>
2026-03-20 15:42:46 -0400

    Implement TUC2 manual tests (#122)

--- tests/server.tests/integration.tests/register.integration.test.ts
+++ tests/server.tests/integration.tests/register.integration.test.ts
@@ -97,17 +97,14 @@ async function request(
 
 beforeAll(async () => {
   const db = new MongoDB(TEST_DB_URL);
-  app = new App(
-    [new HomeController('/'), new AuthController('/auth')],
-    {
-      clientDir: './.dist/client',
-      db,
-      port: TEST_PORT,
-      host: 'localhost',
-      url: TEST_URL,
-      initOnStart: true
-    }
-  );
+  app = new App([new HomeController('/'), new AuthController('/auth')], {
+    clientDir: './.dist/client',
+    db,
+    port: TEST_PORT,
+    host: 'localhost',
+    url: TEST_URL,
+    initOnStart: true
+  });
 
   server = await app.listen();
   await new Promise((resolve) => setTimeout(resolve, 1000));
